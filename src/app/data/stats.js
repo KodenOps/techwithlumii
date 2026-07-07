6406928e3185bc36e651eff2eef41f8fcb0c4fb8
kodenops <segun.femifadiya@gmail.com>
2026-07-07 16:53:45 +0100

    fix: correct cohorts value in stats data

--- src/app/data/stats.js
+++ src/app/data/stats.js
@@ -1,7 +1,7 @@
  const statss = [
   { label: 'Trained Experts', value: 1.4, suffix: 'k+', decimals: 1 },
   { label: 'Avg. completion', value: 95, suffix: '%', decimals: 0 },
-  { label: 'Cohorts', value: 58, suffix: '+', decimals: 0 },
+  { label: 'Cohorts', value: 16, suffix: '+', decimals: 0 },
 ];
 export default statss
 
